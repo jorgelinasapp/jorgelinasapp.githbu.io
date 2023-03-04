501f207a45e7ae61215452e52a93a409cb1bab42
jorgelinasapp <126989877+jorgelinasapp@users.noreply.github.com>
2023-03-04 19:16:49 -0300

    Initial Commit

---
+++ sintaxis.js
@@ -0,0 +1,8 @@
+
+function cambiarColor(){
+    document.body.style.backgroundColor="red" 
+    /* document.getElementById('boton_color').addEventListener('click', function(){debugger
+       /*  document.body.style.backgroundColor= 'red';  
+        document.body.style.backgroundColor="red" 
+    }); */
+}
